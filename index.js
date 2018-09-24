83ccbb6923f6e5e5bbaecafc888d0a03a7291f16
Joseph Parker <jparker010190@gmail.com>
2018-09-24 00:10:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-
+class Driver{
+  constructor(name, startDate){
+    this.name = name
+    this.startDate = new Date()
+    console.log(startDate)
+  }
+  yearsExperienceFromBeginningOf(endDate){
+    const date = new Date()
+    return endDate - 1995
+  }
+}
+class Route {
+  constructor(horizontal, vertical){
+    this.horizontal = horizontal
+    this.vertical = vertical
+  }
+  blocksTravelled(blocks){
+    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+    return this.endingLocation - this.beginningLocation
+  }
+}
